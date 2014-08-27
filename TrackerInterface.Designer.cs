0f0337910f9fb87d4510831941c92d9bb180e13b
enroq <enroqadmin@kairyt.com>
2014-08-27 12:14:40 -0400

    Initial Drop Beta 1.0

---
+++ TrackerInterface.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace VoidTracker
+{
+    partial class TrackerInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrackerInterface));
+            this.TextDisplay = new System.Windows.Forms.RichTextBox();
+            this.PathButton = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.FileNameEntry = new System.Windows.Forms.TextBox();
+            this.FileLabel = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TotalTimeLabel = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.SuspendLayout();
+            // 
+            // TextDisplay
+            // 
+            this.TextDisplay.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.TextDisplay.Location = new System.Drawing.Point(12, 10);
+            this.TextDisplay.Name = "TextDisplay";
+            this.TextDisplay.Size = new System.Drawing.Size(360, 112);
+            this.TextDisplay.TabIndex = 0;
+            this.TextDisplay.Text = "";
+            // 
+            // PathButton
+            // 
+            this.PathButton.BackColor = System.Drawing.Color.DarkGray;
+            this.PathButton.Location = new System.Drawing.Point(249, 157);
+            this.PathButton.Name = "PathButton";
+            this.PathButton.Size = new System.Drawing.Size(60, 25);
+            this.PathButton.TabIndex = 1;
+            this.PathButton.Text = "Set";
+            this.PathButton.UseVisualStyleBackColor = false;
+            this.PathButton.Click += new System.EventHandler(this.PathButton_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(56, 159);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.Size = new System.Drawing.Size(187, 21);
+            this.PathTextBox.TabIndex = 2;
+            // 
+            // FileNameEntry
+            // 
+            this.FileNameEntry.Location = new System.Drawing.Point(56, 128);
+            this.FileNameEntry.Name = "FileNameEntry";
+            this.FileNameEntry.Size = new System.Drawing.Size(187, 21);
+            this.FileNameEntry.TabIndex = 3;
+            // 
+            // FileLabel
+            // 
+            this.FileLabel.AutoSize = true;
+            this.FileLabel.BackColor = System.Drawing.Color.DimGray;
+            this.FileLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.FileLabel.Location = new System.Drawing.Point(12, 131);
+            this.FileLabel.Name = "FileLabel";
+            this.FileLabel.Size = new System.Drawing.Size(39, 14);
+            this.FileLabel.TabIndex = 4;
+            this.FileLabel.Text = "Name:";
+            // 
+            // StopButton
+            // 
+            this.StopButton.BackColor = System.Drawing.Color.DarkGray;
+            this.StopButton.Location = new System.Drawing.Point(297, 229);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 59);
+            this.StopButton.TabIndex = 5;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = false;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.DarkGray;
+            this.StartButton.Location = new System.Drawing.Point(15, 229);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 59);
+            this.StartButton.TabIndex = 6;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.DarkGray;
+            this.loadButton.Location = new System.Drawing.Point(315, 127);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(57, 25);
+            this.loadButton.TabIndex = 7;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.DarkGray;
+            this.newButton.Location = new System.Drawing.Point(249, 127);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(60, 25);
+            this.newButton.TabIndex = 9;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(18, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 14);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Path:";
+            // 
+            // TotalTimeLabel
+            // 
+            this.TotalTimeLabel.AutoSize = true;
+            this.TotalTimeLabel.BackColor = System.Drawing.Color.DimGray;
+            this.TotalTimeLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.TotalTimeLabel.Location = new System.Drawing.Point(248, 196);
+            this.TotalTimeLabel.Name = "TotalTimeLabel";
+            this.TotalTimeLabel.Size = new System.Drawing.Size(61, 14);
+            this.TotalTimeLabel.TabIndex = 11;
+            this.TotalTimeLabel.Text = "Total Time:";
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.BackColor = System.Drawing.Color.DimGray;
+            this.Time.Location = new System.Drawing.Point(315, 196);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(49, 14);
+            this.Time.TabIndex = 12;
+            this.Time.Text = "00:00:00";
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(15, 192);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(102, 18);
+            this.checkBox.TabIndex = 13;
+            this.checkBox.Text = "Active Logging";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.checkBox_CheckedChanged);
+            // 
+            // TrackerInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(385, 300);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.TotalTimeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.FileLabel);
+            this.Controls.Add(this.FileNameEntry);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.PathButton);
+            this.Controls.Add(this.TextDisplay);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TrackerInterface";
+            this.Text = "Void Tracker";
+            this.Load += new System.EventHandler(this.TrackerInterface_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextDisplay;
+        private System.Windows.Forms.Button PathButton;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.TextBox FileNameEntry;
+        private System.Windows.Forms.Label FileLabel;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TotalTimeLabel;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
---
+++ TrackerInterface.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace VoidTracker
+{
+    public partial class TrackerInterface : Form
+    {
+        #region Data
+        enum RecordState
+        {
+            None,
+            Started,
+            Stopped
+        }
+
+        static RecordState state = RecordState.None;
+
+        static Dictionary<DateTime, DateTime> intervals = new Dictionary<DateTime, DateTime>();
+
+        static string directoryPath = "C:\\VoidTracker\\";
+        static string textPath = Path.Combine(directoryPath, "logsheet.txt");
+
+        static string xmlName = "New Log Sheet";
+        static string xmlPath = Path.Combine(directoryPath, xmlName);
+
+        static string settingsPath = Path.Combine(directoryPath, "settings.xml");
+
+        static bool cached = false; static TimeSpan totalTime;
+
+        DateTime startTime; DateTime endTime;
+
+        static bool activeLogging = false;
+        static Point lastMousePosition = new Point();
+        #endregion
+
+        //Constructor
+        public TrackerInterface()
+        {
+            InitializeComponent(); QuerySettings();
+            FileNameEntry.Text = xmlName;
+            PathTextBox.Text = directoryPath;          
+        }
+
+        #region Functions
+        void CreateNewLog(bool overwrite)
+        {
+            if (!(Directory.Exists(directoryPath)))
+                Directory.CreateDirectory(directoryPath);
+
+            xmlName = FileNameEntry.Text;
+            xmlPath = Path.Combine(directoryPath, xmlName.Replace(" ", "_") + ".xml");
+
+            if (File.Exists(xmlPath) && !overwrite)
+            {
+                SendMessage("Error: File with matching name already exists."); return;
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(xmlPath))
+                {
+                    XmlTextWriter xml = new XmlTextWriter(writer);
+
+                    xml.Formatting = Formatting.Indented;
+                    xml.IndentChar = '\t';
+                    xml.Indentation = 1;
+
+                    xml.WriteStartDocument(true);
+                    xml.WriteStartElement("log_file");
+                    xml.WriteAttributeString("Name", xmlName);
+
+                    if (overwrite)
+                    {
+                        foreach (KeyValuePair<DateTime, DateTime> interval in intervals)
+                        {
+                            xml.WriteStartElement("interval");
+                            xml.WriteAttributeString("start", interval.Key.ToString());
+                            xml.WriteAttributeString("stop", interval.Value.ToString());
+                            xml.WriteEndElement();
+                        }
+                    }
+
+                    xml.WriteEndElement();
+                    xml.WriteEndDocument();
+                    xml.Close();
+                }
+
+                if (!overwrite)
+                {
+                    SendMessage("Success: You have created a new log file.");
+                    totalTime = TimeSpan.FromSeconds(0);
+                    Time.Text = totalTime.ToString("hh' : 'mm' : 'ss");
+
+                }
+
+                ModifySettings();
+                FormatTextFile();
+            }
+
+            catch { SendMessage("Error: Unable to create file " + xmlPath); }
+        }
+
+        void SendMessage(string msg)
+        {
+            TextDisplay.Text += "[" + msg + "]";
+            TextDisplay.Text += "\n";
+            TextDisplay.SelectionStart = TextDisplay.Text.Length;
+            TextDisplay.ScrollToCaret();
+        }
+
+        void ModifySettings()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(settingsPath))
+                {
+                    XmlTextWriter xml = new XmlTextWriter(writer);
+
+                    xml.Formatting = Formatting.Indented;
+                    xml.IndentChar = '\t';
+                    xml.Indentation = 1;
+
+                    xml.WriteStartDocument(true);
+                    xml.WriteStartElement("settings_file");
+                    xml.WriteAttributeString("path", directoryPath);
+                    xml.WriteAttributeString("cache", xmlPath);
+                    xml.WriteEndElement();
+                    xml.WriteEndDocument();
+                    xml.Close();
+                }
+            }
+            catch { }
+        }
+
+        void FormatTextFile()
+        {
+            if (!(Directory.Exists(directoryPath)))
+                Directory.CreateDirectory(directoryPath);
+
+            try
+            {   //FileStream stream = new FileStream(fullPath, FileMode.Open, FileAccess.Write, FileShare.Write); ??
+
+                TextWriter writer = new StreamWriter(textPath);
+
+                writer.WriteLine("[" + xmlName + " initialized @: " + DateTime.Now.ToString() + "]");
+
+                foreach (KeyValuePair<DateTime, DateTime> interval in intervals)
+                {
+                    DateTime start = interval.Key;
+                    DateTime end = interval.Value;
+
+                    TimeSpan span = end - start;
+
+                    writer.WriteLine("[Shift Logged: (" + start.ToString() + " to " + end.ToString() + ") Total: (" + span.ToString("hh' : 'mm' : 'ss") + ")]");
+                }
+
+                writer.Close(); writer.Dispose();
+            }
+
+            catch { SendMessage("Error: Unable to write (" + textPath + ")."); }
+
+        }
+
+        void QuerySettings()
+        {
+            if(!(File.Exists(settingsPath)))
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(settingsPath))
+                    {
+                        XmlTextWriter xml = new XmlTextWriter(writer);
+
+                        xml.Formatting = Formatting.Indented;
+                        xml.IndentChar = '\t';
+                        xml.Indentation = 1;
+
+                        xml.WriteStartDocument(true);
+                        xml.WriteStartElement("settings_file");
+                        xml.WriteAttributeString("path", directoryPath);
+                        xml.WriteAttributeString("cache", xmlPath);
+                        xml.WriteEndElement();
+                        xml.WriteEndDocument();
+                        xml.Close();
+                    } 
+                }
+
+                catch { }
+            }
+
+            else if (File.Exists(settingsPath))
+            {
+                XmlDocument doc = new XmlDocument();
+                XmlElement root;
+
+                try
+                {
+                    doc.Load(settingsPath);
+                    root = doc["settings_file"];
+
+                    directoryPath = root.GetAttribute("path");
+                    xmlPath = root.GetAttribute("cache");
+
+                    doc.Load(xmlPath); root = doc["log_file"];
+                    xmlName = FileNameEntry.Text = root.GetAttribute("Name");
+
+                    cached = true;
+
+
+                    foreach (XmlElement element in root.GetElementsByTagName("interval"))
+                    {
+                        DateTime start = DateTime.Parse(element.GetAttribute("start"));
+                        DateTime stop = DateTime.Parse(element.GetAttribute("stop"));
+
+                        intervals.Add(start, stop); totalTime += stop - start;
+                        Time.Text = totalTime.ToString("hh' : 'mm' : 'ss");
+                    }
+                }
+
+                catch { }
+            }
+        }
+
+        void LoadFromXML()
+        {
+            using (OpenFileDialog fileBrowser = new OpenFileDialog())
+            {
+                fileBrowser.FileName = directoryPath;
+                if (fileBrowser.ShowDialog() == DialogResult.OK)
+                {
+                    string extension = System.IO.Path.GetExtension(fileBrowser.FileName);
+
+                    if (extension == ".xml")
+                    {
+                        xmlPath = fileBrowser.FileName;
+
+                        if (File.Exists(xmlPath))
+                        {
+                            XmlDocument doc = new XmlDocument();
+                            XmlElement root;
+
+                            try
+                            {
+                                doc.Load(xmlPath);
+                                root = doc["log_file"];
+
+                                xmlName = FileNameEntry.Text = root.GetAttribute("Name");
+                                xmlPath = Path.Combine(directoryPath, xmlName.Replace(" ", "_") + ".xml");
+
+                                ModifySettings();
+
+                                intervals.Clear(); int count = 0; totalTime = TimeSpan.FromSeconds(0);
+
+                                foreach (XmlElement element in root.GetElementsByTagName("interval"))
+                                {
+                                    DateTime start = DateTime.Parse(element.GetAttribute("start"));
+                                    DateTime stop = DateTime.Parse(element.GetAttribute("stop"));
+
+                                    intervals.Add(start, stop); count++;
+                                    totalTime += stop - start;
+                                    Time.Text = totalTime.ToString("hh' : 'mm' : 'ss");
+                                }
+
+                                SendMessage(String.Format("Loading ({0}) {2} from {1}", count, xmlPath, count > 1 ? "Intervals" : "Interval"));
+                            }
+
+                            catch { SendMessage("Error: Unable to parse xml file."); }
+                        }
+                    }
+
+                    else SendMessage("Error: You must select  an .xml file.");
+
+                    fileBrowser.Dispose();
+                }
+            }
+        }
+
+        void BeginShift()
+        {
+            if (state != RecordState.Started)
+            {
+                state = RecordState.Started;
+                startTime = DateTime.Now;
+
+                SendMessage("Logging Initialized @: " + startTime);
+
+                if (activeLogging)
+                {
+                    MovementTimer timer = new MovementTimer(this);
+                    lastMousePosition = Cursor.Position;
+                }
+            }
+
+            else SendMessage("Error: Logging Already in Process.");
+        }
+
+        void EndShift()
+        {
+            if (state == RecordState.Started)
+            {
+                state = RecordState.Stopped;
+                endTime = DateTime.Now;
+                totalTime += endTime - startTime;
+
+                intervals.Add(startTime, endTime);
+
+                SendMessage("Logging Complete @: " + endTime);
+                CreateNewLog(true);
+
+                TextDisplay.SelectionStart = TextDisplay.Text.Length;
+                TextDisplay.ScrollToCaret();
+
+                Time.Text = totalTime.ToString("hh' : 'mm' : 'ss");       
+            }
+        }
+        #endregion
+
+        #region Events
+        private void TrackerInterface_Load(object sender, EventArgs e)
+        {
+            if (cached)
+                SendMessage("Previous Log Loaded from " + xmlPath);
+        }
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox.Checked)
+            {
+                activeLogging = true;
+                SendMessage("Active Logging Enabled..");
+            }
+
+            else activeLogging = false;
+        }
+
+        private void PathButton_Click(object sender, EventArgs e)
+        {
+            folderBrowser.SelectedPath = directoryPath;
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                directoryPath = folderBrowser.SelectedPath;
+                PathTextBox.Text = directoryPath;
+                folderBrowser.Dispose();
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            LoadFromXML();
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            CreateNewLog(false);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            BeginShift();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            EndShift();
+        }
+        #endregion
+
+        private class MovementTimer
+        {
+            static Timer timer;
+            static TrackerInterface tracker;
+
+            static int Minutes(int i)
+            {
+                return i * 60000;
+            }
+
+            public MovementTimer(TrackerInterface t)
+            {
+                tracker = t;
+
+                timer = new Timer();
+                timer.Interval = Minutes(5);
+
+                timer.Tick += new EventHandler(OnTick);
+                timer.Start();
+            }
+
+            void OnTick(object sender, EventArgs e)
+            {
+                QueryPosition();
+            }
+
+            void QueryPosition()
+            {
+                if (Cursor.Position != lastMousePosition)
+                {
+                    lastMousePosition = Cursor.Position;
+                }
+
+                else if (Cursor.Position == lastMousePosition)
+                {
+                    if (tracker != null)
+                    {
+                        tracker.SendMessage("#! Logging Suspended Due to Inactivity..");
+
+                        tracker.EndShift();
+                        ((Form)tracker).Activate();
+
+                        timer.Stop(); timer.Dispose();                     
+                    }
+                }
+            }
+        }
+    }
+}
